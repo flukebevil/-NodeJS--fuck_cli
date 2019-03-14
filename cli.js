614c94933ef54fbd482a0258a460289ad42745f2
sarawoot pokkam <flukebevil@gmail.com>
2019-03-14 19:08:27 +0700

    update0.1.0

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 const chalk = require("chalk");
 const { spawn } = require("child_process");
 const logUpdate = require("log-update");
+const shelljs = require("shelljs");
 
 const log = console.log;
 const helperSeletion = process.argv;
@@ -24,15 +25,31 @@ const shell = (command, subCommand) => {
 };
 
 if (helperSeletion[2] !== undefined) {
-  shell("git", ["add", "."]).then(_ => {
-    logUpdate(`\n ${chalk.green("✔")} Git added! `);
-    shell("git", ["commit", "-m", helperSeletion[2]]).then(_ => {
-      logUpdate(`\n ${chalk.green("✔")} Git commited! `);
-      shell("git", ["push"]).then(_ => {
-        logUpdate(`\n ${chalk.green("✔")} finished! \n`);
+  if (shelljs.exec("git remote").code !== 0) {
+    log(`\n You need to use follow this first \n`);
+    log(`👉🏻 ${chalk.green("git init")}`);
+    log(`👉🏻 ${chalk.green("git remote add origin <remote repository URL>")}`);
+    log(`👉🏻 ${chalk.green('git commit -m <"commit message">')}`);
+    log(`👉🏻 ${chalk.green("Final git push --set-upstream origin master")}`);
+    log(
+      chalk.blue.bgRed.bold(
+        `\n\n🗣 ${chalk.white(" For next time you can use just ")} ${chalk.green(
+          'fuck <"message"> \n'
+        )}`
+      )
+    );
+    process.exit(1);
+  } else {
+    shell("git", ["add", "."]).then(_ => {
+      logUpdate(`\n ${chalk.green("✔")} Git added! `);
+      shell("git", ["commit", "-m", helperSeletion[2]]).then(_ => {
+        logUpdate(`\n ${chalk.green("✔")} Git commited! `);
+        shell("git", ["push"]).then(_ => {
+          logUpdate(`\n ${chalk.green("✔")} finished! \n`);
+        });
       });
     });
-  });
+  }
 } else {
   log(`
     ${chalk.green("Usage")} : fuck <"message">
